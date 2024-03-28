d0497735d8106997f2cd38bb32422d30d4688cb8
Luiz <luiz_f_maria@estudante.sesisenai.org.br>
2024-03-28 09:42:33 -0300

    cria e destroi inimigos

--- two_main.js
+++ two_main.js
@@ -41,6 +41,85 @@ linha3.des_obj()
 game_over.des_text('Game Over',340, 320, 'White', '55px Pixelify Sans')
 enter.des_text('Pressione Enter para recomeçar', 265, 400, 'White', '26px Pixelify Sans');
 
+//Criação dos inimigos
+let grupoInimigo = []
+let inimigo = {
+    time1: 0, //tempo para os inimigos aparecerem
+    time2: 0, //tempo para os inimigos aparecerem
+    time3: 0, //tempo para os inimigos aparecerem
+
+    criaInimigo(){
+        this.time1 += 0.4
+        this.time2 += 0.4
+        this.time3 += 0.4
+        let pos_x = (Math.random() * (835 - 2 +1)+2)  //onde os inimigos vão aparecer
+        let pos_x2 = (Math.random() * (835 - 2 +1)+2) //onde os inimigos vão aparecer
+        let pos_x3 = (Math.random() * (835 - 2 +1)+2) //onde os inimigos vão aparecer
+
+        if(this.time1 >=60){
+            this.time1 = 0
+            grupoInimigo.push(new Inimigo(pos_x,-200,80,80,'./assets/Morcego_1.png'))
+            console.log(grupoInimigo)
+        }
+        if(this.time2 >=85){
+            this.time2 = 0
+            grupoInimigo.push(new Inimigo(pos_x2,-300,80,80,'./assets/Morcego_2.png'))
+            console.log(grupoInimigo)
+        }
+        if(this.time3 >=135){
+            this.time3 = 0
+            grupoInimigo.push(new Inimigo(pos_x3,-400,80,80,'./assets/Morcego_2.png'))
+            console.log(grupoInimigo)
+        }
+    
+        //Ajuste a criação de inimigos com base no nível atual
+        if (nivel >= 2) {
+            this.time1 += 0.5
+            this.time2 += 0.5
+            this.time3 += 0.5
+
+        }else if(nivel >= 3){
+            this.time1 += 0.6
+            this.time2 += 0.6
+            this.time3 += 0.6
+
+        }else if(nivel >= 4){
+            this.time1 += 0.8
+            this.time2 += 0.8
+            this.time3 += 0.8
+        }
+    },
+    des(){
+        grupoInimigo.forEach((inimigo)=>{ //Desenha o inimigo
+            inimigo.des_img()
+        })
+    },
+
+    //se o tiro pegar no inimigo ele é destruido
+    destroiInimigo(){
+        grupoTiros.forEach((tiro)=>{
+            grupoInimigo.forEach((inimigo)=>{
+                if(tiro.colid(inimigo)){
+                    grupoTiros.splice(grupoTiros.indexOf(tiro), 1)
+                    grupoInimigo.splice(grupoInimigo.indexOf(inimigo), 1)
+                    player.pts += 1
+                    reproduzirAudio(dead)
+                }
+            })
+        })
+    },
+    atual(){
+        this.criaInimigo()
+        this.destroiInimigo()
+        grupoInimigo.forEach((inimigo)=>{ //atualiza o inimigo
+            inimigo.mov()
+            inimigo.anim('Morcego_')
+            inimigo.atual_inimigo()
+        })
+    }
+}
+
+
 if(jogar){
     player.mov()
     player.anim('mago_')
@@ -52,7 +131,6 @@ function desenhaTelaInicio(){
 
     des.clearRect(0, 0, 1300, 600) //cria um quadrado para a tela de inicio
 
-    bg_inicio.des_img()
 
     //faz o text
     piece.des_text('PIECE', 620, 300, 'White', '80px Pixelify Sans')
