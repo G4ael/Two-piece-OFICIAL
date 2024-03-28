f105e424b8aa8c8f3b1b295ec406991aa785814f
JOÃO GABRIEL DOS SANTOS <joao_santos31@estudante.sesisenai.org.br>
2024-03-28 09:37:00 -0300

    Img, Audios, Class OBJ, Readme adicionados

---
+++ two_class.js
@@ -0,0 +1,21 @@
+
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+    des_obj(){
+
+    }
+
+    des_img(){
+
+    }
+
+    colid(objeto) {
+
+        }
+    }
